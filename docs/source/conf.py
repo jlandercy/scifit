334abad0cf75cb62a03495b2d31f0ae39082eba3
jeanlandercy <jeanlandercy@live.com>
2023-08-20 07:20:08 +0200

    Version change 0.1.4 -> 0.1.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2023, Jean Landercy'
 author = 'Jean Landercy'
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.4"
+release = "0.1.5"
 
 
 # -- General to_dict ---------------------------------------------------
