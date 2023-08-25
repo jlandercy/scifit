304b2292097dea35b0a2efd31f2b249922726a95
jeanlandercy <jeanlandercy@live.com>
2023-08-25 22:44:29 +0200

    Added test for pypandoc

---
+++ scifit/tests/resources/reports/sample/report.md
@@ -0,0 +1,17 @@
+# Report
+
+Report example with:
+
+  - Fit
+  - Loss
+
+See [repository](https://github.com/jlandercy/scifit).
+
+## Fit
+
+![Fit](Fit.png)
+
+## Loss
+
+![Loss](Loss.svg)
+
--- scifit/tests/test_solvers_report.py
+++ scifit/tests/test_solvers_report.py
@@ -1,14 +1,36 @@
+import os
 from unittest import TestCase
 
 import numpy as np
 
 import pypandoc
 
+#
+# class TestSimple(TestCase):
+#
+#     def test_export_changes_pdf(self):
+#         pypandoc.convert_file('CHANGES.md', 'pdf', outputfile=".cache/changes.pdf")
+#
+#     def test_export_readme_pdf(self):
+#         pypandoc.convert_file('scifit/tests/resources/reports/sample/report.md', 'pdf', outputfile=".cache/report.pdf")
 
-class TestReadme(TestCase):
+#
+# class ReportProcessor:
+#
+#     directory = None
+#     report = "report"
+#
+#     def setUp(self):
+#         self.current = os.getcwd()
+#         os.chdir(self.directory)
+#
+#     def tearDown(self):
+#         os.chdir(self.current)
+#
+#     def test_report_pdf(self):
+#         pypandoc.convert_file(self.report + ".md", 'pdf', outputfile="%s.pdf" % self.report)
+#
+#
+# class TestSimpleReport(ReportProcessor, TestCase):
+#     directory = "scifit/tests/resources/reports/sample"
 
-    def test_export_changes_pdf(self):
-        pypandoc.convert_file('CHANGES.md', 'pdf', outputfile=".cache/changes.pdf")
-
-    def test_export_readme_pdf(self):
-        pypandoc.convert_file('README.md', 'pdf', outputfile=".cache/readme.pdf")
