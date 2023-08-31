30e292c770378ae7f33ef6ad75c0176e5d014566
jeanlandercy <jeanlandercy@live.com>
2023-09-01 00:17:55 +0200

    Looking for oscillating brusselator

--- scifit/solvers/kinetics.py
+++ scifit/solvers/kinetics.py
@@ -37,3 +37,33 @@ class SequenceKineticSolver(FitSolver1D):
     def model(cls, x, b0, b1):
         solution = cls.kinetic.solve(x[:, 0], [b0, b1], None).y.T[:, 1]
         return solution
+
+
+class BrusselatorKineticSolver(FitSolver1D):
+
+    kinetic = KineticSolverInterface(
+        nur=np.array(
+            [
+                [-1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, -2, -1],
+                [0, -1, 0, 0, -1, 0],
+                [0, 0, 0, 0, -1, 0],
+            ]
+        ),
+        nup=np.array(
+            [
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 3, 0],
+                [0, 0, 1, 0, 0, 1],
+                [0, 0, 0, 1, 0, 0],
+            ]
+        ),
+        k0=np.array([1.0, 1.0, 1.0, 1.0]),
+        x0=np.array([1.0, 3.0, 0.0, 0.0, 1.0, 1.0]),
+        steady=np.array([0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
+    )
+
+    @classmethod
+    def model(cls, x, b0, b1, b2, b3):
+        solution = cls.kinetic.solve(x[:, 0], [b0, b1, b2, b3], None).y.T[:, 4]
+        return solution
--- scifit/tests/test_solvers_kinetics.py
+++ scifit/tests/test_solvers_kinetics.py
@@ -21,44 +21,66 @@ class GenericKineticRegression(GenericTestFitSolver):
     def test_kolmogorov(self):
         pass
 
+#
+# class SimpleKineticRegression(GenericKineticRegression):
+#     factory = kinetics.SimpleKineticSolver
+#     parameters = np.array([2.1e-2])
+#     configuration = {"p0": np.array([1e-3])}
+#     xmin = 0.0
+#     xmax = 500.0
+#     resolution = 50
+#
+#
+# class SimpleKineticRegressionNoiseL0(SimpleKineticRegression, TestCase):
+#     sigma = 1e-6
+#
+#
+# class SimpleKineticRegressionNoiseL1(SimpleKineticRegression, TestCase):
+#     sigma = 2.5e-2
+#
+#
+# class SimpleKineticRegressionNoiseL2(SimpleKineticRegression, TestCase):
+#     sigma = 1e-1
+#
+#
+# class SequenceKineticRegression(GenericKineticRegression):
+#     factory = kinetics.SequenceKineticSolver
+#     parameters = np.array([2.1e-2, 3.15e-3])
+#     configuration = {"p0": np.array([2e-2, 2e-3])}
+#     xmin = 0.0
+#     xmax = 1000.0
+#     resolution = 50
+#
+#
+# class SequenceKineticRegressionNoiseL0(SequenceKineticRegression, TestCase):
+#     sigma = 1e-6
+#
+#
+# class SequenceKineticRegressionNoiseL1(SequenceKineticRegression, TestCase):
+#     sigma = 2.5e-2
+#
+#
+# class SequenceKineticRegressionNoiseL2(SequenceKineticRegression, TestCase):
+#     sigma = 1e-1
+#
 
-class SimpleKineticRegression(GenericKineticRegression):
-    factory = kinetics.SimpleKineticSolver
-    parameters = np.array([2.1e-2])
-    configuration = {"p0": np.array([1e-3])}
+
+class BrusselatorKineticRegression(GenericKineticRegression):
+    factory = kinetics.BrusselatorKineticSolver
+    parameters = np.array([1.1, 0.9, 1.1, 0.9])
+    #configuration = {"p0": np.array([2e-2, 2e-3])}
     xmin = 0.0
-    xmax = 500.0
-    resolution = 50
+    xmax = 50.0
+    resolution = 75
 
 
-class SimpleKineticRegressionNoiseL0(SimpleKineticRegression, TestCase):
+class BrusselatorKineticRegressionNoiseL0(BrusselatorKineticRegression, TestCase):
     sigma = 1e-6
 
 
-class SimpleKineticRegressionNoiseL1(SimpleKineticRegression, TestCase):
+class BrusselatorKineticRegressionNoiseL1(BrusselatorKineticRegression, TestCase):
     sigma = 2.5e-2
 
 
-class SimpleKineticRegressionNoiseL2(SimpleKineticRegression, TestCase):
-    sigma = 1e-1
-
-
-class SequenceKineticRegression(GenericKineticRegression):
-    factory = kinetics.SequenceKineticSolver
-    parameters = np.array([2.1e-2, 3.15e-3])
-    configuration = {"p0": np.array([2e-2, 2e-3])}
-    xmin = 0.0
-    xmax = 1000.0
-    resolution = 50
-
-
-class SequenceKineticRegressionNoiseL0(SequenceKineticRegression, TestCase):
-    sigma = 1e-6
-
-
-class SequenceKineticRegressionNoiseL1(SequenceKineticRegression, TestCase):
-    sigma = 2.5e-2
-
-
-class SequenceKineticRegressionNoiseL2(SequenceKineticRegression, TestCase):
+class BrusselatorKineticRegressionNoiseL2(BrusselatorKineticRegression, TestCase):
     sigma = 1e-1
